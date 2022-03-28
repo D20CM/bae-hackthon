7104707c75fab88e646c9c552d8bc2bb2e0a499c
ajp64 <antonyjparnell@gmail.com>
2022-03-28 13:23:28 +0100

    progress page set up

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
+import { Progress } from "./components/Progress/Progress";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Progress />
   </React.StrictMode>,
   document.getElementById("root")
 );
