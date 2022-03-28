df8452f2bc94ce6c86353f398ec6980c3c6448ba
Rahrahrarara <92094786+Rahrahrarara@users.noreply.github.com>
2022-03-28 14:14:02 +0100

    landing page done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,11 @@
 import logo from "../../logo.svg";
+import Landing from "../Landing/landing";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>BAE Hackathon</p>
-      </header>
+      <Landing />
     </div>
   );
 }
