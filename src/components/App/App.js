9b91b946d00a067a084587b2856c71bdeb56c354
ajp64 <antonyjparnell@gmail.com>
2022-03-28 13:36:02 +0100

    react router set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,31 @@
 import logo from "../../logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Progress } from "../Progress/Progress";
+//import { EmergingReader } from "../EmergingReader/EmergingReader";
+//import { Profile } from "../Profile/Profile";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>BAE Hackathon</p>
+        <p>LearnIt!</p>
       </header>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/progress">Progress</Link> |{" "}
+        <Link to="/emergingreader">Emerging Reader</Link>
+      </nav>
+      <Routes>
+        {/* <Route path="profile" element={<Profile />} /> */}
+        {/* <Route path="emergingreader" element={<EmergingReader />} /> */}
+        <Route path="progress" element={<Progress />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App/App";
-import { Progress } from "./components/Progress/Progress";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Progress />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
